27dc25f78a51ac9ee19e7568ab691389c53b5789
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-02-02 02:23:54 +0700

    Add new Function TambahProduk

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Produk struct {
+	KodeProduk string `json:"kodeproduk"`
+	NamaProduk string `json:"namaproduk"`
+	Kuantitas string `json:"kuantitas"`
+}
+
+type Keranjang struct {
+	Produk []Produk
+}
+
+var keranjang = &Keranjang{}
+
+func (k *Keranjang) TambahProduk(produk Produk)  {
+	for i, p := range k.Produk {
+		if p.KodeProduk == produk.KodeProduk {
+			k.Produk[i].Kuantitas += produk.Kuantitas
+			return
+		}
+	}
+	k.Produk = append(k.Produk, produk)
+}
+
+func main() {
+	
+}
