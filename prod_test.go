ff878359c06d99b8bdd8fc2483acd30babb7b084
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-02-02 13:06:15 +0700

    Menambahkan unit test Tampilkan Produk

--- prod_test.go
+++ prod_test.go
@@ -54,3 +54,37 @@ func TestHapusProduk(t *testing.T) {
 	assert.Equal(t, "Sepatu Balet", keranjang.Prod[0].NamaProduk)
 	assert.Equal(t, 50, keranjang.Prod[0].Kuantitas)
 }
+
+func TestTampilkanProduk(t *testing.T) {
+	keranjang := &Keranjang{
+		Prod: []Produk{
+			{
+				KodeProduk: "A001",
+				NamaProduk: "Kunci Gantung",
+				Kuantitas: 200,
+			},
+			{
+				KodeProduk: "A002",
+				NamaProduk: "Sepatu Bola",
+				Kuantitas: 250,
+			},
+			{
+				KodeProduk: "A003",
+				NamaProduk: "Lampu Bola",
+				Kuantitas: 25,
+			},
+		},
+	}
+
+	produk := keranjang.TampilkanKeranjang("", 0)
+	assert.Len(t, produk, 3)
+	assert.Equal(t, "A001", produk[0].KodeProduk)
+	assert.Equal(t, "Kunci Gantung", produk[0].NamaProduk)
+	assert.Equal(t, 200, produk[0].Kuantitas)
+	assert.Equal(t, "A002", produk[1].KodeProduk)
+	assert.Equal(t, "Sepatu Bola", produk[1].NamaProduk)
+	assert.Equal(t, 250, produk[1].Kuantitas)
+	assert.Equal(t, "A003", produk[2].KodeProduk)
+	assert.Equal(t, "Lampu Bola", produk[2].NamaProduk)
+	assert.Equal(t, 25, produk[2].Kuantitas)
+}
