a9a0acf07c8a443f68e1afae1874c8352161fe07
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-02-02 13:21:20 +0700

    Menambahkan fungsi oneEditReplace

--- Soal1_2/compare/compare.go
+++ Soal1_2/compare/compare.go
@@ -19,3 +19,17 @@ func oneEditInsert(s1, s2 string) bool {
 
 	return true
 }
+
+func oneEditReplace(s1, s2 string) bool {
+	foundDiff := false
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			if foundDiff {
+				return false
+			}
+			foundDiff = true
+		}
+	}
+	return true
+}
+
