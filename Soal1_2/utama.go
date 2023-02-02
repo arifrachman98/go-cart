e5751d890acfdd92fec6b7e10f4dff54d238e6b5
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-02-02 13:30:16 +0700

    Melengkapi program utama

--- Soal1_2/utama.go
+++ Soal1_2/utama.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+	"go-cart/Soal1_2/compare"
+	"go-cart/Soal1_2/pecahan"
 	"os"
 
 	"os/exec"
 	"runtime"
-
-	"project_takehome/compare"
-	"project_takehome/pecahan"
 )
 
 func clearScreen() {
@@ -23,5 +22,33 @@ func clearScreen() {
 }
 
 func main() {
-	
+	clearScreen()
+	pilihan := 0
+
+	fmt.Println("1. Menentukan Pecahan Uang")
+	fmt.Println("2. Mengetahui string dapat diedit atau tidak")
+	fmt.Println("3. Exit")
+	fmt.Println("=========================================================")
+
+	for {
+
+		fmt.Print("Masukan Pilihan Anda : ")
+		fmt.Scanln(&pilihan)
+
+		switch pilihan {
+		case 1:
+			pecahan.Pecahan()
+			fmt.Println()
+		case 2:
+			compare.Compare()
+			fmt.Println()
+		case 3:
+			fmt.Print("See You . . .")
+			os.Exit(0)
+		default:
+			fmt.Println("Input yang anda masukan salah!")
+			os.Exit(0)
+		}
+
+	}
 }
